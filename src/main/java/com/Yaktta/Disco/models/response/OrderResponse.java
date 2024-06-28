b075dd00e8b567c3686071403a68df51eb8be00a
Brandt322 <bgamboaa98@gmail.com>
2024-06-27 22:13:37 -0500

    feat: Get orders and orders detail

--- src/main/java/com/Yaktta/Disco/models/response/OrderResponse.java
+++ src/main/java/com/Yaktta/Disco/models/response/OrderResponse.java
@@ -1,10 +1,23 @@
 package com.Yaktta.Disco.models.response;
 
+import com.Yaktta.Disco.models.entities.OrderDetail;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
+import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderResponse {
+    private Long idOrder;
+    private Long clientId;
+    private Date orderDate;
+    private String description;
+    private double amount;
+    private List<OrderDetail> orderDetails;
 }
